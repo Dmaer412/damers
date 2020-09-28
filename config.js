558a1172d24810094131e93edf7c96445a69aa6b
Dmaer412 <72026182+Dmaer412@users.noreply.github.com>
2020-09-28 16:19:43 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NzU5ODQyNzM3NTk1NjEzMjA2.X3DYsQ.CTj26QioKuG1lsZouu9J1itdFiY", //تعديل مهم توكن بوتك
+  YT_API_KEY: "api ",
+  prefix: "#",
+  devs: ["729800829044785263"] ///تعديل مهم غير هذا الرقم بـ ايدي حسابك
+};
